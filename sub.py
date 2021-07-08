424c82ef5830e818dfe79102e47c5293b2d5ad00
AnonTheDrunk <sufle11@gmail.com>
2021-07-08 12:54:48 +0300

    add a few buttons and preparing for calculetions

--- sub.py
+++ sub.py
@@ -1,6 +1,6 @@
 from tkinter import *
 
-def MA(x):
+'''def MA(x):
     if x == '+':
         plus(a,b)
     elif x == '-':
@@ -14,9 +14,9 @@ def MA(x):
             return 0
 
 def inputs(a):
-    aa = a
+    aa = [int(a)]
     #return aa
-    print(aa)
+    
 
 def plus(a,b):
     return a + b
@@ -28,28 +28,63 @@ def mult(a,b):
     return a * b
 
 def div(a,b):
-    return a / b
+    return a / b '''
 
 root = Tk()
 root.title("Калькулятор")
 root.geometry("600x400")
 
-btn1 = Button(root, text="1", width=3, height=2, command=inputs(1))
-btn2 = Button(root, text="2", width=3, height=2, command=inputs(1))
-btn3 = Button(root, text="3", width=3, height=2, command=inputs(1))
-btn4 = Button(root, text="4", width=3, height=2, command=inputs(1))
-btn5 = Button(root, text="5", width=3, height=2, command=inputs(1))
-btn6 = Button(root, text="6", width=3, height=2, command=inputs(1))
-btn7 = Button(root, text="7", width=3, height=2, command=inputs(1))
-btn8 = Button(root, text="8", width=3, height=2, command=inputs(1))
-btn9 = Button(root, text="9", width=3, height=2, command=inputs(1))
-btn0 = Button(root, text="0", width=3, height=2, command=inputs(1))
-btnP = Button(root, text="+", width=3, height=2, command=inputs(1))
-btnM = Button(root, text="-", width=3, height=2, command=inputs(1))
-btnMult = Button(root, text="*", width=3, height=2, command=inputs(1))
-btnDiv = Button(root, text="/", width=3, height=2, command=inputs(1))
+frame1 = Frame(root)
+frame2 = Frame(root)
 
-btn.pack(side="left")
+
+btn1 = Button(frame1, text="1", width=10, height=6)#, command=inputs(1))
+btn2 = Button(frame1, text="2", width=10, height=6)#, command=inputs(2))
+btn3 = Button(frame1, text="3", width=10, height=6)#, command=inputs(3))
+btn4 = Button(frame1, text="4", width=10, height=6)#, command=inputs(4))
+btn5 = Button(frame1, text="5", width=10, height=6)#, command=inputs(5))
+btn6 = Button(frame1, text="6", width=10, height=6)#, command=inputs(6))
+btn7 = Button(frame1, text="7", width=10, height=6)#, command=inputs(7))
+btn8 = Button(frame1, text="8", width=10, height=6)#, command=inputs(8))
+btn9 = Button(frame1, text="9", width=10, height=6)#, command=inputs(9))
+btn0 = Button(frame1, text="0", width=10, height=6)#, command=inputs(0))
+btnP = Button(frame2, text="+", width=10, height=6)#, command=MA('+'))
+btnM = Button(frame2, text="-", width=10, height=6)#, command=MA('-'))
+btnMult = Button(frame2, text="*", width=10, height=6)#, command=MA('*'))
+btnDiv = Button(frame2, text="/", width=10, height=6)#, command=MA('/'))
+btnE = Button(frame1, text="=", width=21, height=6)
+
+'''btn1.pack(side="center")
+btn2.pack(side="center")
+btn3.pack(side="center")
+btn4.pack(side="center")
+btn5.pack(side="center")
+btn6.pack(side="center")
+btn7.pack(side="center")
+btn8.pack(side="center")
+btn9.pack(side="center")'''
+
+
+
+frame1.pack(side=LEFT, fill="x")
+frame2.pack(side=LEFT, fill="x")
+
+btnP.grid(row=0,column=0)
+btnM.grid(row=0,column=1)
+btnMult.grid(row=1,column=0)
+btnDiv.grid(row=1,column=1)
+
+btn1.grid(row=0,column=0)
+btn2.grid(row=0,column=1)
+btn3.grid(row=0,column=2)
+btn4.grid(row=1,column=0)
+btn5.grid(row=1,column=1)
+btn6.grid(row=1,column=2)
+btn7.grid(row=2,column=0)
+btn8.grid(row=2,column=1)
+btn9.grid(row=2,column=2)
+btn0.grid(row=3,column=0)
+btnE.grid(row=3,column=1, columnspan=2)
 
 
 
